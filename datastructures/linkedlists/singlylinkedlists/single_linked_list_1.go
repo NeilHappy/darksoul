3ea3cf16484999aeda4b53336afa920596b458bb
neilhappy <neilhappyneil@gmail.com>
2023-09-20 16:24:39 +0800

    feat: init

---
+++ datastructures/linkedlists/singlylinkedlists/single_linked_list_1.go
@@ -0,0 +1,66 @@
+package singlylinkedlists
+
+import "errors"
+
+var (
+	errEmptyList        = errors.New("list is empty")
+	errIndexOutOfBounds = errors.New("index out of bounds")
+)
+
+// SLLNode the node struct of a linked_list
+type SLLNode[T any] struct {
+	Value T
+	Next  *SLLNode[T] // singly linked_list only has next pointer
+}
+
+// SinglyLinkedList structure
+type SinglyLinkedList[T any] struct {
+	count int
+	head  *SLLNode[T]
+	tail  *SLLNode[T]
+}
+
+func (s *SinglyLinkedList[T]) Add(values ...T) {
+	for _, value := range values {
+		s.InsertAt(s.Size(), value)
+	}
+}
+
+func (s *SinglyLinkedList[T]) Size() int {
+	return s.count
+}
+
+func (s *SinglyLinkedList[T]) InsertAt(index int, value T) error {
+	if index < 0 || index > s.count {
+		return errIndexOutOfBounds
+	}
+	element := &SLLNode[T]{Value: value}
+	if index == s.count {
+		s.tail.Next = element
+		s.tail = element
+		s.count++
+		element = nil
+		return nil
+	}
+
+	return nil
+}
+
+func (s *SinglyLinkedList[T]) IsEmpty() bool {
+	return s.Size() == 0
+}
+
+func (s *SinglyLinkedList[T]) GetFirstValue() (res T, err error) {
+	if !s.IsEmpty() {
+		return s.head.Value, nil
+	} else {
+		return res, errEmptyList
+	}
+}
+
+// New generate a singly linked list
+func New[T any](values ...T) *SinglyLinkedList[T] {
+	s := &SinglyLinkedList[T]{}
+	s.Add(values...)
+	return s
+}
