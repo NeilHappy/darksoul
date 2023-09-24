d0f383f7c52bbc10740029617e8beba761e85e49
neilhappy <neilhappyneil@gmail.com>
2023-09-24 19:02:09 +0800

    feat: lt

---
+++ code/35.search-insert-position.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=35 lang=golang
+ *
+ * [35] Search Insert Position
+ */
+package main
+
+// @lc code=start
+func searchInsert(nums []int, target int) int {
+	left := 0
+	right := len(nums)
+	for left < right {
+		mid := left + (right-left)>>1
+		if target == nums[mid] {
+			return mid
+		} else if target > nums[mid] {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	return left
+}
+
+// @lc code=end
---
+++ code/todo/34.find-first-and-last-position-of-element-in-sorted-array.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=34 lang=golang
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ */
+package main
+
+/*
+
+
+
+
+
+
+ */
+
+// @lc code=start
+func searchRange(nums []int, target int) []int {
+	return nil
+}
+
+/*
+
+
+
+
+
+
+ */
+
+// @lc code=end
